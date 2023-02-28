cae56995ada76a072ed758e2d376981ac2939ca0
andrii_h <galgayand@gmail.com>
2023-02-28 21:22:01 +0200

    controllers

---
+++ controllers/auth/avatarUpdate.js
@@ -0,0 +1,26 @@
+const fs = require('fs/promises');
+const { path } = require('path');
+const { User } = require('../../models/user');
+
+const avatarDir = path.join(__dirname, '../..', 'public', 'avatars');
+
+const avatarUpdate = async (req, res) => {
+  try {
+    const { _id } = req.user;
+    const { path: tempUpload, originalname } = req.name;
+    const extension = originalname.split('.').pop();
+    const filename = `${_id}.${extension}`;
+    const resultUpload = path.join(avatarDir, filename);
+    await fs.rename(tempUpload, resultUpload);
+    const avatarURL = path.join('avatars', filename);
+    await User.findByIdAndUpdate(_id, { avatarURL: avatarDir });
+    res.json({
+      avatarURL,
+    });
+  } catch (error) {
+    await fs.unlink(req.file.path);
+    throw error;
+  }
+};
+
+module.exports = avatarUpdate;
